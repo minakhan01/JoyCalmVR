b7aa5ec70af85493e9055561ca8417001a5a86da
minakhan01 <minakhan01@gmail.com>
2016-07-05 13:37:00 -0400

    all files

---
+++ Assets/Nature_FX/Scripts/BackButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackButton : MonoBehaviour {
+
+	void Awake() 
+	{
+		GetComponent<GvrViewer>().OnBackButton += HandleOnBackButton;
+	}
+
+	void HandleOnBackButton ()
+	{
+		Application.Quit();        
+	}
+}
